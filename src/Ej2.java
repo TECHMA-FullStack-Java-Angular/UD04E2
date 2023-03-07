bb21d1a4b6061bd2ca06c122e56561199e31437d
Paula Sousa <paulasousa137@gmail.com>
2023-03-07 12:13:25 +0100

    Ejercicio2

---
+++ src/Ej2.java
@@ -0,0 +1,17 @@
+
+public class Ej2 {
+	public static void main (String args[]) {
+		int n=1;
+		double a=1.2;
+		char c='A';
+		
+		System.out.println("Variable n es " +n);
+		System.out.println("Variable a es " +a);
+		System.out.println("Variable c es "+c);
+		System.out.println("La suma de n + a es "+(n+a));
+		System.out.println("La resta de a - n es "+(a-n));
+		System.out.println("El valor numerico de c es "+Character.getNumericValue(c));
+		
+	}
+
+}
